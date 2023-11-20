9a5a2bc3a87a8c253d89ca8918d501d731445bb2
17TheWord <17theword@gmail.com>
2023-11-20 16:27:26 +0800

    更新基本返回体中data变量的属性

--- src/main/java/com/github/theword/returnBody/MinecraftReturnBody.java
+++ src/main/java/com/github/theword/returnBody/MinecraftReturnBody.java
@@ -1,15 +1,17 @@
 package com.github.theword.returnBody;
 
+import com.google.gson.JsonElement;
+
 public class MinecraftReturnBody {
 
     private String api;
 
-    private String data;
+    private JsonElement data;
 
     public MinecraftReturnBody() {
     }
 
-    public MinecraftReturnBody(String api, String data) {
+    public MinecraftReturnBody(String api, JsonElement data) {
         this.api = api;
         this.data = data;
     }
@@ -30,11 +32,11 @@ public class MinecraftReturnBody {
         this.api = api;
     }
 
-    public String getData() {
+    public JsonElement getData() {
         return data;
     }
 
-    public void setData(String data) {
+    public void setData(JsonElement data) {
         this.data = data;
     }
 }
