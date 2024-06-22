b2caa77b4bc445482705c8ffa372d1349b543a57
17TheWord <17theword@gmail.com>
2024-06-22 15:13:14 +0800

    :sparkles: Enhance message handling by adding prefix to TextComponent

--- src/main/java/com/github/theword/mcqq/handleMessage/HandleApiService.java
+++ src/main/java/com/github/theword/mcqq/handleMessage/HandleApiService.java
@@ -4,6 +4,7 @@ import com.github.theword.mcqq.returnBody.returnModle.MyBaseComponent;
 import com.github.theword.mcqq.returnBody.returnModle.MyTextComponent;
 import com.github.theword.mcqq.returnBody.returnModle.SendTitle;
 import com.github.theword.mcqq.utils.ParseJsonToEvent;
+import com.github.theword.mcqq.utils.Tool;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
@@ -19,7 +20,8 @@ public class HandleApiService implements HandleApi {
 
     @Override
     public void handleBroadcastMessage(WebSocket webSocket, List<MyTextComponent> messageList) {
-        TextComponent textComponent = parseJsonToEvent.parseMessageToTextComponent(messageList);
+        TextComponent textComponent = parseJsonToEvent.parsePerMessageToTextComponent(Tool.getPrefixComponent());
+        textComponent.addExtra(parseJsonToEvent.parseMessageToTextComponent(messageList));
         instance.getServer().spigot().broadcast(textComponent);
     }
 
@@ -38,9 +40,9 @@ public class HandleApiService implements HandleApi {
 
     @Override
     public void handleActionBarMessage(WebSocket webSocket, List<MyBaseComponent> messageList) {
-        TextComponent actionTextComponent = parseJsonToEvent.parseMessageToTextComponent(messageList);
+        TextComponent actionbarTextComponent = parseJsonToEvent.parseMessageToTextComponent(messageList);
         for (Player player : instance.getServer().getOnlinePlayers()) {
-            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, actionTextComponent);
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, actionbarTextComponent);
         }
     }
 }
